4462ed3620948517531ebc498470a862602bfb8a
gabriela.berte <you@example.com>
2021-10-13 10:12:50 -0300

    adicionando link

--- src/App.js
+++ src/App.js
@@ -2,36 +2,31 @@ import './App.css';
 import ListaPessoas from './components/ListaPessoas';
 import Header from './layout/Header';
 
-<<<<<<< HEAD
-const pessoas = [
-  { id: 1, nome: 'Guilherme' },
-  { id: 2, nome: 'Otávio' },
-  { id: 3, nome: 'Júlio' },
-  { id: 4, nome: 'Júlio' },
-  { id: 4, nome: 'Gabe' }
-=======
 const gentes = [
   {
     id: 1,
     nome: 'Guilherme',
-    perfil: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique facilis deleniti, vel quidem quam cum magni dignissimos debitis temporibus repudiandae consequatur, modi placeat explicabo alias voluptates rerum architecto! Alias, rerum!'
+    perfil: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique facilis deleniti, vel quidem quam cum magni dignissimos debitis temporibus repudiandae consequatur, modi placeat explicabo alias voluptates rerum architecto! Alias, rerum!',
+    link: 'github.com/gentes.nome'
   },
   {
     id: 2,
     nome: 'Otávio',
-    perfil: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis corrupti nisi, cumque necessitatibus fugiat tenetur! Vero quam ea eos? Explicabo aliquam dolorem esse pariatur mollitia, quasi nesciunt cum quisquam quod?'
+    perfil: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis corrupti nisi, cumque necessitatibus fugiat tenetur! Vero quam ea eos? Explicabo aliquam dolorem esse pariatur mollitia, quasi nesciunt cum quisquam quod?',
+    link: 'github.com/gentes.nome'
   },
   {
     id: 3,
     nome: 'Júlio',
-    perfil: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consequatur autem architecto accusantium! Ipsa dolor in laboriosam, a fugit sapiente labore cumque ducimus minus. Quisquam quidem ducimus praesentium fuga provident.'
+    perfil: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consequatur autem architecto accusantium! Ipsa dolor in laboriosam, a fugit sapiente labore cumque ducimus minus. Quisquam quidem ducimus praesentium fuga provident.',
+    link: 'github.com/gentes.nome'
   },
   {
     id: 4,
     nome: 'Júlio',
-    perfil: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, laudantium voluptatem ea maiores enim quam molestiae consequuntur expedita modi minus nam consectetur cupiditate nostrum iste dicta, excepturi qui quia distinctio!'
+    perfil: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, laudantium voluptatem ea maiores enim quam molestiae consequuntur expedita modi minus nam consectetur cupiditate nostrum iste dicta, excepturi qui quia distinctio!',
+    link: 'github.com/gentes.nome'
   },
->>>>>>> 2f140d99d4bf9e5180ff303bbb99202abddd4467
 ];
 
 const headerDetails = {
